5269b72871804a8e1fcee94e7d832ca3730e4350
JStrimpel576 <JoshStrimpel@outlook.com>
2024-11-25 21:47:35 -0500

    Final Project Milestone 1 Complete
    -Created a Database filled with dummy data
    
    -Added other classes and activities.

--- app/DatabaseHelper.java
+++ app/DatabaseHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper
 {
     private static final String database_name = "GiftList.db";
-    private static final String giftRecievers_table_name = "giftRecievers";
+    private static final String giftReceivers_table_name = "giftReceivers";
     private static final String gifts_table_name = "Gifts";
     private static final String category_table_name = "Categories";
 
@@ -18,15 +18,15 @@ public class DatabaseHelper extends SQLiteOpenHelper
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        db.execSQL("CREATE TABLE " + giftRecievers_table_name + " (gifteeId integer primary key autoincrement not null, fname varchar(50), lname varchar(50), giftCount integer);");
-        db.execSQL("CREATE TABLE " + gifts_table_name + " (giftId integer primary key autoincrement not null, cost float, categoryId integer, giftName varchar(50), gifteeId integer, foreign key (categoryId) references " + category_table_name + " (categoryId), foreign key (gifteeId) references " + giftRecievers_table_name + " (gifteeId));");
+        db.execSQL("CREATE TABLE " + giftReceivers_table_name + " (gifteeId integer primary key autoincrement not null, fname varchar(50), lname varchar(50), giftCount integer);");
+        db.execSQL("CREATE TABLE " + gifts_table_name + " (giftId integer primary key autoincrement not null, gifteeId integer, categoryId integer, giftName varchar(50), cost float, foreign key (categoryId) references " + category_table_name + " (categoryId), foreign key (gifteeId) references " + giftReceivers_table_name + " (gifteeId));");
         db.execSQL("CREATE TABLE " + category_table_name + " (categoryId integer primary key autoincrement not null, giftType varchar(50));");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1)
     {
-        db.execSQL("DROP TABLE IF EXISTS " + giftRecievers_table_name + ";");
+        db.execSQL("DROP TABLE IF EXISTS " + giftReceivers_table_name + ";");
         db.execSQL("DROP TABLE IF EXISTS " + gifts_table_name + ";");
         db.execSQL("DROP TABLE IF EXISTS " + category_table_name + ";");
 
@@ -35,7 +35,7 @@ public class DatabaseHelper extends SQLiteOpenHelper
 
     public String getGiftRecieversDbName()
     {
-        return giftRecievers_table_name;
+        return giftReceivers_table_name;
     }
 
     public String getGiftsDbName()
@@ -50,29 +50,28 @@ public class DatabaseHelper extends SQLiteOpenHelper
 
     public void initAllTables()
     {
-        initGiftRecievers();
+        initGiftReceivers();
         initGifts();
         initCategories();
     }
 
-    private void  initGiftRecievers()
+    private void initGiftReceivers()
     {
-        if(countRecordsFromTable(giftRecievers_table_name) == 0)
+        if(countRecordsFromTable(giftReceivers_table_name) == 0)
         {
             SQLiteDatabase db = this.getWritableDatabase();
 
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Joshua', 'Strimpel', 5);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('James', 'Smith', 7);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('John', 'Struts', 6);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Jason', 'True', 4);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Truce', 'Harding', 5);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Mark', 'Strimpel', 4);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Will', 'Bill', 4);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Rube', 'Shorting', 2);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Dean', 'Smith', 6);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Rich', 'Struts', 7);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Tilly', 'Bill', 1);");
-            db.execSQL("INSERT INTO " + giftRecievers_table_name + "(fname, lname, giftCount) VALUES ('Trudy', 'True', 6);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('Joshua', 'Strimpel', 2);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('James', 'Smith', 2);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('John', 'Struts', 2);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('Jason', 'True', 2);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('Truce', 'Harding', 1);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('John', 'Ranting', 1);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('Will', 'Bill', 2);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('Rube', 'Shorting', 1);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('Dean', 'Smith', 2);");
+            db.execSQL("INSERT INTO " + giftReceivers_table_name + "(fname, lname, giftCount) VALUES ('Rich', 'Struts', 1);");
+
 
             db.close();
         }
@@ -84,7 +83,54 @@ public class DatabaseHelper extends SQLiteOpenHelper
         {
             SQLiteDatabase db = this.getWritableDatabase();
 
-            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, giftName, cost) VALUES (1, )");
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (1, 1, 'Random Video Game', 59.99);");
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (1, 6, 'Drumset', 349.99);");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (2, 8, 'Pillow', 14.59);");
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (2, 8, 'Blanket', 12.35);");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (3, 4, 'Hammer', 14.99)");
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (3, 4, 'Screwdriver', 12.99)");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (4, 3, 'Weights', 13.99);");
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (4, 2, 'Baseball', 9.99);");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (5, 5, 'McDonalds Gift Card', 99.99);");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (6, 7, 'Recliner', 149.99);");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (7, 9, 'Tablet', 99.99);");
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (7, 9, 'Camera', 149.99);");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (8, 10, 'Table', 79.99);");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (9, 4, 'Ruler', 8.99);");
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (9, 6, 'Guitar', 146.99)");
+
+            db.execSQL("INSERT INTO " + gifts_table_name + "(gifteeId, categoryId, giftName, cost) VALUES (10, 5, 'Home Depot Gift Card', 99.99)");
+
+            db.close();
+        }
+    }
+
+    private void initCategories()
+    {
+        if(countRecordsFromTable(category_table_name) == 0)
+        {
+            SQLiteDatabase db = this.getWritableDatabase();
+
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Game');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Sport');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Exercise');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Gadget');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('GiftCard');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Instrument');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Furniture');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Bedding');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Technology');");
+            db.execSQL("INSERT INTO " + category_table_name + "(giftType) VALUES ('Appliance');");
+
+            db.close();
         }
     }
 
